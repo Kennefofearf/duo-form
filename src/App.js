06cf1ce644a35949402ab3cabb248b5560d27887
Kennefofearf <kpeterson344@gmail.com>
2023-09-13 19:14:12 -0400

    Began to create the form

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import ReactDOM from 'react-dom/client';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function MyForm() {
+  const [inputs, setInputs] = useState({});
+
+  const submissionValues = (event) => {
+    event.preventDefault();
+    alert(`Name entered was: ${name}`)
+  }
+
+  return()
 }
 
 export default App;
