9978c5c920fa2b75713857f33a5a616f54402a8e
Kennefofearf <kpeterson344@gmail.com>
2023-09-28 22:48:14 -0400

    Added a comment

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App () {
     return (
       <div className="App">
         <header className="App-header">
+          {/* Creating the form */}
           <form>
       <label>
         First Name:
